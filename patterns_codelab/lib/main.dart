6cdbe44bd1eac8d328cde29fe19574a8e3ae8ce2
Hung Nguyen <hungnb94@gmail.com>
2025-10-01 15:05:08 +0700

    Use object patterns

--- patterns_codelab/lib/main.dart
+++ patterns_codelab/lib/main.dart
@@ -25,10 +25,66 @@ class DocumentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final (title, :modified) = document.metadata;
+    final formattedModifiedDate = formatDate(modified);
+    final blocks = document.getBlocks();
 
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: Column(children: [Center(child: Text('Last modified $modified'))]),
+      body: Column(
+        children: [
+          Text('Last modified $formattedModifiedDate'),
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({required this.block, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      child: switch (block) {
+        HeaderBlock(:final text) => Text(
+          text,
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+        ParagraphBlock(:final text) => Text(text),
+        CheckboxBlock(:final text, :final isCheck) => Row(
+          children: [
+            Checkbox(value: isCheck, onChanged: (_) {}),
+            Text(text),
+          ],
+        ),
+      },
+    );
+  }
+}
+
+String formatDate(DateTime dateTime) {
+  final today = DateTime.now();
+  final difference = dateTime.difference(today);
+
+  return switch (difference) {
+    Duration(inDays: 0) => 'today',
+    Duration(inDays: 1) => 'tomorrow',
+    Duration(inDays: -1) => 'yesterday',
+    Duration(inDays: final days) when days > 7 => '${days ~/ 7} weeks from now',
+    Duration(inDays: final days) when days < -7 =>
+      '${days.abs() ~/ 7} weeks ago',
+    Duration(inDays: final days, isNegative: true) => '${days.abs()} days ago',
+    Duration(inDays: final days) => '$days days from now',
+  };
 }
